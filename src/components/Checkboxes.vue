a0dcece4c17b2bfd1df2861eb1b22828c9d3bd2d
Bibiana Sebestianova <b.sebestianova@gmail.com>
2022-03-14 12:02:22 +0100

    feat: filtering overview - added filtering logic to overview page

---
+++ src/components/Checkboxes.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  entries: Object,
+  onChange: Function,
+});
+</script>
+
+<template>
+  <div :key="entry.id" v-for="entry of entries">
+    <input
+      v-on:change="onChange($event)"
+      type="checkbox"
+      :id="entry.id"
+      :name="entry.value"
+    />
+    <label :for="entry.id"> {{ entry.name }}</label>
+  </div>
+</template>
--- src/components/blogs/BlogPreviews.vue
+++ src/components/blogs/BlogPreviews.vue
@@ -1,21 +1,63 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref, watchEffect } from "vue";
 import { useStoryblokApi } from "@storyblok/vue";
 import BlogPreview from "./BlogPreview.vue";
+import Checkboxes from "../Checkboxes.vue";
+
+defineProps({
+  blok: Object,
+});
 
 const storyblokApi = useStoryblokApi();
 const stories = ref(null);
-const { data } = await storyblokApi.get("cdn/stories", {
-  version: "draft",
-  starts_with: "blogs/blog",
-  sort_by: "published_at:asc",
-  resolve_links: "url",
+let filter = [];
+
+const retrieveStory = async (filter) => {
+  return storyblokApi.get("cdn/stories", {
+    version: "draft",
+    starts_with: "blogs/blog",
+    sort_by: "published_at:desc",
+    resolve_links: "url",
+    filter_query: {
+      categories: {
+        in_array: filter ? filter?.join(",") : null,
+      },
+    },
+  });
+};
+
+const { data: datasource_data } = await storyblokApi.get(
+  "cdn/datasource_entries",
+  {
+    datasource: "categories",
+  }
+);
+
+const setStories = async (filter) => {
+  const { data } = await retrieveStory(filter);
+  stories.value = data.stories;
+};
+
+onMounted(async () => {
+  await setStories();
 });
 
-stories.value = data.stories;
+const onChange = async (ev) => {
+  if (ev.target.checked) {
+    filter = [...filter, ev.target.name];
+  } else {
+    filter = filter.filter((val) => val !== ev.target.name);
+  }
+  await setStories(filter);
+};
 </script>
 
 <template>
+  <Checkboxes
+    :entries="datasource_data.datasource_entries"
+    :onChange="onChange"
+  />
+
   <ul class="flex flex-wrap relative">
     <li :key="blok.uuid" v-for="blok in stories">
       <BlogPreview :blok="blok.content" :link="blok.full_slug" />
